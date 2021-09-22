2352c0d847f559e8d9e87b45e3d6d634e26ffe6d
LuisRang <luisfrangel94@gmail.com>
2021-09-21 19:20:00 -0500

    Primer commit

---
+++ deptos.js
@@ -0,0 +1,142 @@
+
+
+
+const capitales  = () =>{
+    let nombredepto = prompt ("Ingrese en nombre del departamento");
+
+switch (nombredepto) {
+        case "amazonas":
+        alert ("Su capital es: Leticia");
+        
+        break;
+
+        case "antioquia":
+        alert ("Su capital es: Medellin");
+        break;
+
+        case "arauca":
+        alert ("Su capital es: Arauca");
+        break;
+
+        case "atlantico":
+        alert ("Su capital es: Barranquilla");
+        break;
+
+        case "bolivar":
+        alert ("Su capital es: Cartagena");
+        break;
+
+        case "boyaca":
+        alert ("Su capital es: Tunja");
+        break;
+
+        case "caldas":
+        alert ("Su capital es: Manizales");
+        break;
+
+        case "caqueta":
+        alert ("Su capital es: Florencia");
+        break;
+
+        case "casanare":
+        alert ("Su capital es: Yopal");
+        break;
+
+        case "cauca":
+        alert ("Su capital es: Popayan");
+        break;
+
+        case "cesar":
+        alert ("Su capital es: Valledupar");
+        break;
+
+        case "choco":
+        alert ("Su capital es: Quibdo");
+        break;
+
+        case "cordoba":
+        alert ("Su capital es: Monteria");
+        break;
+
+        case "cundinamarca":
+        alert ("Su capital es: Bogota");
+        break;
+
+        case "aguainia":
+        alert ("Su capital es: Inirida");
+        break;
+
+        case "guaviare":
+        alert ("Su capital es: San jose del Guaviare");
+        break;
+
+        case "huila":
+        alert ("Su capital es: Neiva");
+        break;
+
+        case "guajira":
+        alert ("Su capital es: Rioacha");
+        break;
+
+        case "magdalena":
+        alert ("Su capital es: Santa Marta");
+        break;
+
+        case "meta":
+        alert ("Su capital es: Villavicencio");
+        break;
+
+        case "nariño":
+        alert ("Su capital es: Pasto");
+        break;
+
+        case "norte de santander":
+        alert ("Su capital es: Cucuta");
+        break;
+
+        case "putumayo":
+        alert ("Su capital es: Mocoa");
+        break;
+
+        case "quindio":
+        alert ("Su capital es: Armenia");
+        break;
+
+        case "risaralda":
+        alert ("Su capital es: Pereira");
+        break;
+
+        case "san andres":
+        alert ("Su capital es: San Andres");
+        break;
+
+        case "santander":
+        alert ("Su capital es: Bucaramanga");
+        break;
+
+        case "sucre":
+        alert ("Su capital es: Sincelejo");
+        break;
+
+        case "tolima":
+        alert ("Su capital es: Ibague");
+        break;
+
+        case "valle":
+        alert ("Su capital es: Cali");
+        break;
+
+        case "vaupes":
+        alert ("Su capital es: Miú");
+        break;
+
+        case "vichada":
+        alert ("Su capital es: Puerto Careño");
+        break;
+        
+    default:
+        alert("No es un departamento valido");
+        break;
+    }
+
+}
